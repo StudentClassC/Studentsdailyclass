580f69891aaa92ff27d6a09796ebbe458f3f2fce
SaleemTahiri <saleem.maz@outlook.com>
2021-11-09 21:06:38 -0600

    Upload Multiple Files using Fileupload

---
+++ SchoolProject/WebForm/MultiFileUpload2.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/WebForm/Site1.Master" AutoEventWireup="true" CodeBehind="MultiFileUpload2.aspx.cs" Inherits="SchoolProject.WebForm.MultiFileUpload2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="../App_Themes/css/myDesign.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:FileUpload ID="FileUpload1" runat="server" AllowMultiple="True" /> <br />
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" /> <br />
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+
+</asp:Content>
---
+++ SchoolProject/WebForm/MultiFileUpload2.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+
+namespace SchoolProject.WebForm
+{
+    public partial class MultiFileUpload2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(FileUpload1.HasFiles)
+            {
+                string PhysicalPath = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(PhysicalPath))
+                    Directory.CreateDirectory(PhysicalPath);
+                int filecount = 0;
+                foreach(HttpPostedFile file in FileUpload1.PostedFiles)
+                {
+                    filecount += 1;
+                    file.SaveAs(PhysicalPath + file.FileName);
+
+                }  
+                Label1.Text = filecount + " files uploaded";
+            }
+        }
+    }
+}
