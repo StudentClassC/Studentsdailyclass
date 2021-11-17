17eda21ac742717884037d9983ee6bdf7f694714
SaleemTahiri <saleem.maz@outlook.com>
2021-11-16 18:16:17 -0600

    Download File

---
+++ SchoolProject/WebForm/myFileDownload.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/WebForm/Site1.Master" AutoEventWireup="true" CodeBehind="myFileDownload.aspx.cs" Inherits="SchoolProject.WebForm.myFileDownload" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="../App_Themes/css/myDesign.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:label ID="label1" Text="Please click the following button to download" runat="server"></asp:label> <br />
+    <asp:Button ID="Button1" runat="server" Text="Download my file" OnClick="Button1_Click" />
+</asp:Content>
---
+++ SchoolProject/WebForm/myFileDownload.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SchoolProject.WebForm
+{
+    public partial class myFileDownload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //hysicalPath = Server.MapPath("~/Uploads/");
+            string filePath = Server.MapPath("~/Uploads/header.jpg");
+            FileInfo file = new FileInfo(filePath);
+            if (file.Exists)
+            {
+                // Clear Reponse reference
+                Response.Clear();
+                // Add header by specifying file name
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+                // Add header for content length
+                Response.AddHeader("Content-Length", file.Length.ToString());
+                // Specify content type
+                Response.ContentType = "text/plain";
+
+                // clear flush
+                Response.Flush();
+                //Transmitting file
+
+                Response.TransmitFile(file.FullName);
+                Response.End();
+
+            }
+            else
+
+                label1.Text = "Request file is not available to download";
+        }
+    }
+}
